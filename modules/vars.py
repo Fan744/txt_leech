6a79dc335c1423f89352d1d8a1542827966fe689
Fan744 <135441979+Fan744@users.noreply.github.com>
2025-04-19 12:15:41 +0530

    Update vars.py

--- modules/vars.py
+++ modules/vars.py
@@ -1,3 +1,3 @@
-api_id = "29953693"
-api_hash = "bc080dadf0a0e8ca544ac842bcfcf2e0"
-bot_token = "7182994277:AAGz3vbjIweEe1qxENkorYiwf99Lk3aXSOA"
+api_id = "28962746"
+api_hash = "727457f88d661b08e636188a949cd9f3"
+bot_token = "7855291529:AAFhZlf3ixZjyZdqYAxvt0P9JpQOeAPceYs"
